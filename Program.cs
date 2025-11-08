22db23f162a41014e0b486ab2fa08712277e2fbd
Jessica <jpanndora@gmail.com>
2025-11-08 00:20:58 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double tamanhoBytes, velocidadeMbps, tempoMin;
+
+Console.Write("Digite o tamanho do arquivo em bytes: ");
+tamanhoBytes = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a velocidade da conexão em Mbps: ");
+velocidadeMbps = Convert.ToDouble(Console.ReadLine());
+tempoMin = tamanhoBytes * 8 / velocidadeMbps / 60;
+Console.WriteLine($"O tempoestimado para o donwload é de {tempoMin:F2} minutos.");
+
